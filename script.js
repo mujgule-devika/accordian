71885ef72dd74ee9d0045ba5fc0477a7acd750ea
Devika Mujgule <mujgule.d@northeastern.edu>
2024-04-13 19:10:42 -0400

    form and data table with search

---
+++ script.js
@@ -0,0 +1,9 @@
+`use strict`
+
+const accordian = document.getElementsByClassName('container')
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
